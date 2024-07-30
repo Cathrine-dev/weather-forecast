04ae19cbe3c104d68efdd75c5577aac67a9d1eef
Cathrine-dev <cathrinekamasho@gmail.com>
2024-07-30 13:21:08 +0200

    added advanced javaScript technics,the weather API and API intergration

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ function refreshWeather(response) {
     windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
     timeElement.innerHTML=formatDate (date);
     temperatureElement.innerHTML = Math.round (temperature);
-
+    
+    getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -67,3 +68,49 @@ searchCity (searchInput.value);
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+function formatDay (timestamp) {
+    let date = new Date (timestamp * 1000);
+    let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+    return days [date.getDay ()];
+}
+
+function getForecast (city) {
+    let apiKey ="0eb691o00e3fb24a210b62tf2c42a9e3";
+    let apiUrl =`https://api.shecodes.io/weather/v1/forecast?query=${city}}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast (response) {
+    console.log(response.data);
+    
+    let forecastHtml="";
+
+    response.data.daily.forEach (function (day,index) {
+        if (index < 5) {
+        forecastHtml =
+        forecastHtml +
+        `
+        <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img src ="${day.condition.icon_url}" class="weather-forecast-icon"/>
+        <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}&deg</strong>
+            </div> 
+            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}&deg</div>
+    </div>
+   </div>
+    `;
+      }
+    });
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML=forecastHtml;
+ 
+}
+
+
+
+
